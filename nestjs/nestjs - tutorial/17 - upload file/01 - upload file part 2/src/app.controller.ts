c0b8519fbd6bbaf97070cf5a600bd7a61a01024e
unknown <mohseen_abasi@yahoo.com>
2024-01-13 20:44:12 +0330

    part 2 upload code done

--- nestjs/nestjs - tutorial/17 - upload file/01 - upload file part 2/src/app.controller.ts
+++ nestjs/nestjs - tutorial/17 - upload file/01 - upload file part 2/src/app.controller.ts
@@ -1,13 +1,25 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { resolve } from 'path';
+import { join } from 'path';
+
+import path = require('path');
 
 @Controller()
 export class AppController {
@@ -19,8 +31,91 @@ export class AppController {
   }
 
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('image'))
-  uploadFile(@Body() body: any, @UploadedFile() image: Express.Multer.File) {
+  // * add object for option
+  @UseInterceptors(
+    FileInterceptor('image', {
+      //* storge for save image
+      storage: diskStorage({
+        destination: resolve(__dirname, '../', 'uploads'),
+        filename: (req, file, cb) => {
+          const filename = path.parse(file.originalname).name;
+          const extension = path.parse(file.originalname).ext;
+          return cb(null, `${filename}-${Date.now()}${extension}`);
+        },
+      }),
+      //* fileFilter for (.png , .pdf , .jpeg) and so on ..
+      fileFilter: (req, file, cb) => {
+        const extension = path.parse(file.originalname).ext;
+
+        if (extension !== '.png') {
+          return cb(new BadRequestException('must be pngs'), false);
+        }
+
+        return cb(null, true);
+      },
+      limits: { fileSize: 2000000 },
+    }),
+  )
+  uploadFile(
+    @Body() body: any,
+    // @UploadedFile(
+    //   //* have bug get us error but img is be uploaded
+    //   new ParseFilePipe({
+    //     validators: [new MaxFileSizeValidator({ maxSize: 1000 })],
+    //   }),
+    // )
+    @UploadedFile()
+    image: Express.Multer.File,
+  ) {
     console.log(body, image);
+    return { message: 'your file is uploaded' };
+  }
+
+  @Post('/Multiupload')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'avatar', maxCount: 1 },
+        { name: 'background', maxCount: 1 },
+      ],
+      {
+        //* storge for save image
+        storage: diskStorage({
+          destination: resolve(__dirname, '../', 'uploads'),
+          filename: (req, file, cb) => {
+            const filename = path.parse(file.originalname).name;
+            const extension = path.parse(file.originalname).ext;
+            return cb(null, `${filename}-${Date.now()}${extension}`);
+          },
+        }),
+        //* fileFilter for (.png , .pdf , .jpeg) and so on ..
+        fileFilter: (req, file, cb) => {
+          const extension = path.parse(file.originalname).ext;
+          console.log(file, 'file');
+          if (extension === '.png' || extension === '.jpeg') {
+            return cb(null, true);
+          }
+
+          return cb(new BadRequestException('must be png or jpeg'), false);
+        },
+        limits: { fileSize: 2000000 },
+      },
+    ),
+  )
+  uploadMultiFiles(
+    @UploadedFiles()
+    files: {
+      avatar?: Express.Multer.File[];
+      background?: Express.Multer.File[];
+    },
+  ) {
+    console.log(files);
+  }
+
+  //* this take image new add for part2 upload
+  @Get('/image/:imagename')
+  showImage(@Param('imagename') imgename: string, @Res() res) {
+    const imagePath = join(__dirname, '..', 'uploads', imgename);
+    return res.sendFile(imagePath);
   }
 }
